e35edce246593f1b7363d486d25e2b208e25d1b6
Héctor H. Arieta <hector.h.arieta@gmail.com>
2021-06-18 01:20:13 -0500

    Se han hecho modificaciones en Practica17. Se agrego un menú que permite al usuario selecionar las acciones a realizar en determinado archivo. Corrección de código y mejoras en el rendimiento del bucle doWhile

--- Practica17/src/itc/App.java
+++ Practica17/src/itc/App.java
@@ -6,26 +6,53 @@ NOTA: estas clases realizan lectura únicamente caracter por caracter
 import javax.swing.JOptionPane;
 import java.io.IOException;
 
+
 public class App {
+
+    public static ManipularArchivo menu(int sel){
+        ManipularArchivo file = null;
+        switch (sel){
+            case 1:
+                try {
+                    file = new ManipularArchivo();
+                    file.setDirArchivo(JOptionPane.showInputDialog("Ruta del archivo"));
+                    file.leerArchivo();
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+                break;
+            case 2:
+                try {
+                    file = new ManipularArchivo();
+                    file.setDirArchivo(JOptionPane.showInputDialog("Ruta del archivo"));
+                    file.setFrase(JOptionPane.showInputDialog("Escriba un frase"));
+                    file.escribirArchivo();
+                }catch (IOException e){
+                    JOptionPane.showMessageDialog(null, e.getMessage());
+                }
+                break;
+        }
+        return file;
+    }
     public static void main(String[] args) {
+        int sel = 0;
+        boolean salir = false;
 
-        ManipularArchivo file = new ManipularArchivo();
+        do {
+            try {
 
-        file.setDirArchivo(JOptionPane.showInputDialog("Ruta del archivo"));
+              sel = Integer.parseInt(JOptionPane.showInputDialog("1 - Leer archivo\n" +
+                      "2 - Escribir archivo\n" +
+                      "3 - salir"));
+              if(sel == 3){
+                  salir = true;
+              }
+              menu(sel);
 
-
-        try {
-            file.setFrase(JOptionPane.showInputDialog("Escriba un frase"));
-            file.escribirArchivo();
-        }catch (IOException e){
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
-
-        try {
-            file.leerArchivo();
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, e.getMessage());
-        }
+            }catch (NumberFormatException e){
+                JOptionPane.showMessageDialog(null, "Error tipo de dato");
+            }
+        }while (!salir);
 
     }
 }
